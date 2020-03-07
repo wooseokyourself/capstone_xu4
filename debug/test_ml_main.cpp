0ec4a58369a1254707fd853ce2acc68a51506ea8
CHOIWOOSEOK <21500740@handong.edu>
2020-03-07 23:33:10 +0900

    debug

--- debug/test_ml_main.cpp
+++ debug/test_ml_main.cpp
@@ -3,7 +3,7 @@
 int main (void) {
     OpenCV_DNN dnn;
     for (int i=1; i<=5; i++) {
-        string TEST_IMAGE_PATH = "test_images/" + to_string(i) + ".jpeg";
+        string TEST_IMAGE_PATH = "debug/test_images/" + to_string(i) + ".jpeg";
         dnn.MachineLearning (TEST_IMAGE_PATH);
     }
     return 0;
