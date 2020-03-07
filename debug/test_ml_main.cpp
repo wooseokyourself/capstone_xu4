600400fdd65a3e04fccd9ac8b7a671a25ec23e77
CHOIWOOSEOK <21500740@handong.edu>
2020-03-07 23:11:18 +0900

    debug

---
+++ debug/test_ml_main.cpp
@@ -0,0 +1,10 @@
+#include "../src/ml.hpp"
+
+int main (void) {
+    OpenCV_DNN dnn;
+    for (int i=1; i<=5; i++) {
+        string TEST_IMAGE_PATH = "test_images/" + to_string(i) + ".jpeg";
+        dnn.MachineLearning (TEST_IMAGE_PATH);
+    }
+    return 0;
+}
