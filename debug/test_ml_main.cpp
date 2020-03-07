3aa8f681816199fea4575b2b0f276e2ab67944eb
CHOIWOOSEOK <21500740@handong.edu>
2020-03-08 02:30:02 +0900

    debug

--- debug/test_ml_main.cpp
+++ debug/test_ml_main.cpp
@@ -1,13 +1,14 @@
 #include "../src/ml.hpp"
 
 int main (int argc, char* argv[]) {
-    if (argc != 2) {
-        printf ("./dnn.out <테스트할 이미지 파일의 수>\n");
+    if (argc != 3) {
+        printf ("./dnn.out <처음: 테스트할 이미지 파일> <끝: 테스트할 이미지 파일>\n");
         return 0;
     }
-    int imgNum = atoi(argv[1]);
+    int imgStart = atoi(argv[1]);
+    int imgEnd = atoi(argv[2]);
     OpenCV_DNN dnn;
-    for (int i=1; i<=imgNum; i++) {
+    for (int i=imgStart; i<=imgEnd; i++) {
         string TEST_IMAGE_PATH = "debug/test_images/" + to_string(i) + ".jpeg";
         dnn.MachineLearning (TEST_IMAGE_PATH);
     }
