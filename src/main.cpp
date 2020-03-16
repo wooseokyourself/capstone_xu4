c28d4e14f5e9b32a2425fe6fc2c7f79c4d40c8b9
CHOIWOOSEOK <21500740@handong.edu>
2020-03-17 01:33:52 +0900

    update

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <string>
+
+#include "ml.hpp"
+#include "socket.hpp"
+
+
+int main (void) {
+    OpenCV_DNN dnn;
+    while (true) {
+        std::vector<unsigned char> dataPtr = RecvBuffer ();
+		dnn.MachineLearning (dataPtr);
+    }
+    return 0;
+}
