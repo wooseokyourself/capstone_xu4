d58f1bd99f54c6b25ad948e71f4d8531ccc171b8
CHOIWOOSEOK <21500740@handong.edu>
2020-05-28 15:02:22 +0900

    debugging

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -7,26 +7,26 @@
 int
 main (int argc, char* argv[]) {
     printf ("start program\n");
-    config_data conf_data;
+    config_data _conf_data;
     printf ("conf sync!\n");
-    conf_data.sync(); // 서비스 최초 실행시 제일 먼저 conf_data를 읽어온다.
+    _conf_data.sync(); // 서비스 최초 실행시 제일 먼저 conf_data를 읽어온다.
     printf ("conf sync done!\n");
-    OpenCV_DNN dnn (conf_data.resize_res, conf_data.confThreshold, conf_data.nmsThreshold);
+    OpenCV_DNN dnn (_conf_data.resize_res, _conf_data.confThreshold, _conf_data.nmsThreshold);
     printf ("dnn done !\n");
     Uploader ups;
 
-    io_data _io_data (conf_data.camera_number);
+    io_data _io_data (_conf_data.camera_number);
 
     int MODE_FLAG = BASIC_MODE;
     int WORK_FLAG = GO_TAKE_PICTURE;
 
     std::mutex m;
-    std::thread cam_thr (camera_handler, std::ref(_io_data), std::ref(conf_data.camera_number),
+    std::thread cam_thr (camera_handler, std::ref(_io_data), std::ref(_conf_data),
                         std::ref(WORK_FLAG), std::ref(MODE_FLAG), std::ref(m)); // 소켓통신 시작
     int dummy = 0;
     while (true) {
-        if (conf_data.sync()) // prev==ADMIN && now==BASIC 이면 config 갱신
-            dnn.update (conf_data);
+        if (_conf_data.sync()) // prev==ADMIN && now==BASIC 이면 config 갱신
+            dnn.update (_conf_data);
         if (WORK_FLAG == DONE_TAKE_PICTURE) { // 사진촬영을 모두 완료하였다면
             printf (" WORK_FLAG: DONE_TAKE_PICTURE --> GO_INFERENCE\n");
             m.lock();
