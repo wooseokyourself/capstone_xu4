6d40e9241013b027b99c853a5d1493c5575a3060
CHOIWOOSEOK <21500740@handong.edu>
2020-05-31 12:10:15 +0900

    debugging

--- src/cpp/multi_video_main.cpp
+++ src/cpp/multi_video_main.cpp
@@ -3,64 +3,48 @@
 #include "uploader.hpp"
 #include "admin.hpp"
 
+string __root_path;
+
 /* 어플 최초 실행시 conf_data를 웹에서 입력받은 뒤에 이 C++ 프로그램이 실행되어야 config data를 초기화할 수 있음 */
 int
 main (int argc, char* argv[]) {
+    __root_path = string(argv[1]);
     printf ("start program\n");
-    config_data conf_data;
+    config_data _conf_data;
     printf ("conf sync!\n");
     // conf_data.sync(); // 서비스 최초 실행시 제일 먼저 conf_data를 읽어온다.
-    conf_data.camera_number = 1;
-    conf_data.capture_res.width = 416;
-    conf_data.capture_res.height = 416;
-    conf_data.resize_res.width = 416;
-    conf_data.resize_res.height = 416;
+    _conf_data.sync();
     printf ("conf sync done!\n");
-    OpenCV_DNN dnn (conf_data.resize_res, conf_data.confThreshold, conf_data.nmsThreshold);
+    OpenCV_DNN dnn (_conf_data);
     printf ("dnn done !\n");
     Uploader ups;
 
-    io_data _io_data (conf_data.camera_number);
+    io_data _io_data (_conf_data.camera_number);
 
     int MODE_FLAG = BASIC_MODE;
     int WORK_FLAG = GO_TAKE_PICTURE;
 
     std::mutex m;
-    printf ("1\n"); 
-    std::thread cam_thr (camera_handler, std::ref(_io_data.imgs), std::ref(conf_data.camera_number), 
-                        std::ref(WORK_FLAG), std::ref(MODE_FLAG), std::ref(m)); // 소켓통신 시작
-    printf ("2\n");
+    std::thread cam_thr (camera_handler, std::ref(_io_data), std::ref(_conf_data),
+                        std::ref(WORK_FLAG), std::ref(MODE_FLAG), std::ref(m));
+
     int dummy = 0;
     while (true) {
-        printf ("3\n");
-        if (conf_data.sync()) { 
-            printf ("4\n");
-            dnn.update (conf_data);
-            printf ("5\n");
-            if (WORK_FLAG == DONE_TAKE_PICTURE) { // 사진촬영을 모두 완료하였다면
-                printf ("6\n");
-                m.lock();
-                WORK_FLAG = GO_INFERENCE;
-                m.unlock();
-                printf ("7\n");
-                ups.upload_input (_io_data);
-                printf ("8\n");
-                dnn.inference(_io_data);
-                printf ("9\n");
-                ups.upload_output (_io_data);
-                printf ("10\n");
-
-                m.lock();
-                WORK_FLAG = GO_TAKE_PICTURE; // 다시 사진촬영 요청
-                m.unlock();
-            }
-            else {// 사진촬영중이므로 대기
-                dummy++;
-            }
+        if (_conf_data.sync()) // prev==ADMIN && now==BASIC 이면 config 갱신
+            dnn.update (_conf_data);
+        if (WORK_FLAG == DONE_TAKE_PICTURE) { // 사진촬영을 모두 완료하였다면
+            printf (" WORK_FLAG: DONE_TAKE_PICTURE --> GO_INFERENCE\n");
+            m.lock();
+            WORK_FLAG = GO_INFERENCE;
+            ups.upload_input (_io_data);
+            dnn.inference(_io_data);
+            ups.upload_output (_io_data);
+            WORK_FLAG = GO_TAKE_PICTURE; // 다시 사진촬영 요청
+            m.unlock();
+            printf (" WORK_FLAG: DONE_TAKE_PICTURE --> GO_TAKE_PICTURE\n");
         }
-        else { // Admin Mode이므로 대기
+        else// 사진촬영중이므로 대기
             dummy++;
-        }
     }
     return 0;
 }
